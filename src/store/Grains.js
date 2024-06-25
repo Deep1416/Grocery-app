7b00df48d90fd2070f5b331f780997c718d6982d
Deependra <deependra186037@gamil.com>
2024-06-25 21:06:12 +0530

    added some fruits

---
+++ src/store/Grains.js
@@ -0,0 +1,50 @@
+export const Grains = [
+  {
+    category: "Grains",
+    items: [
+      {
+        name: "Rice",
+        img: "https://media.istockphoto.com/id/1401261369/photo/white-raw-rice.webp?b=1&s=170667a&w=0&k=20&c=DjAwcuhRnSypv1nvsONghNKQnKfZ4ybiznzea-IKp_g=",
+        id: 16,
+        price: 4.99,
+        quantity: 1,
+        unit: "kg",
+        reviews: 4.7,
+        reviewCount: 23,
+      },
+      {
+        name: "Wheat",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjtmU1s_ffVdZgs1Slz1BI8-1-g-uN1jjtV9C-isIuch_zJ2_ledVjgG9ad6HobkHZzk4&usqp=CAU",
+        id: 17,
+        price: 3.99,
+
+        quantity: 1,
+        unit: "kg",
+        reviews: 4.5,
+        reviewCount: 21,
+      },
+      {
+        name: "Oats",
+        img: "https://media.istockphoto.com/id/599793642/photo/pile-of-oatmeal.jpg?s=612x612&w=0&k=20&c=Lcb4GDaGj1TYSm842C47wn2eW9h27gBKZiDEtGwE4q0=",
+        id: 18,
+        price: 2.99,
+
+        quantity: 1,
+        unit: "kg",
+        reviews: 4.3,
+        reviewCount: 17,
+      },
+      {
+        name: "Barley",
+        img: "https://thumbs.dreamstime.com/b/pearled-barley-white-background-16848134.jpg",
+        id: 19,
+        price: 3.49,
+
+        quantity: 1,
+        unit: "kg",
+        reviews: 4.4,
+        reviewCount: 19,
+      },
+    ],
+  },
+];
